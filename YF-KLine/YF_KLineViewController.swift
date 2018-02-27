0122d6a49eed4b40401d1d77aefa9cb5b8c1930f
tsaievan <tsaievan@icloud.com>
2018-02-27 16:53:47 +0800

    实现StockChartView的数据源方法

--- YF-KLine/YF_KLineViewController.swift
+++ YF-KLine/YF_KLineViewController.swift
@@ -15,6 +15,32 @@ class YF_KLineViewController: UIViewController {
     }
 }
 
+extension YF_KLineViewController: YF_StockChartViewDataSource {
+    func getStockDatas(index: Int) {
+        var type: String?
+        switch index {
+        case 0:
+            type = "1min"
+        case 1:
+            type = "1min"
+        case 2:
+            type = "1min"
+        case 3:
+            type = "5min"
+        case 4:
+            type = "30min"
+        case 5:
+            type = "60min"
+        case 6:
+            type = "1day"
+        case 7:
+            type = "1week"
+        default: break
+        }
+        reloadData()
+    }
+}
+
 extension YF_KLineViewController {
     
     ///< 加载数据
---
+++ YF-KLine/YF_StockChartView.swift
@@ -0,0 +1,20 @@
+//
+//  YF_StockChartView.swift
+//  YF-KLine
+//
+//  Created by tsaievan on 27/2/18.
+//  Copyright © 2018年 tsaievan. All rights reserved.
+//
+
+import UIKit
+
+///< YF_StockChartView的数据源
+protocol YF_StockChartViewDataSource {
+    func getStockDatas(index: Int)
+}
+
+class YF_StockChartView: UIView {
+
+
+
+}
