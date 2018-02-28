e909a4c0bafb73e65d4e88d037e2f9dfc62db678
tsaievan <tsaievan@icloud.com>
2018-02-28 12:53:42 +0800

    写一点StockChartSegmentView的布局等等

--- YF-KLine/Views/YF_StockChartSegmentView.swift
+++ YF-KLine/Views/YF_StockChartSegmentView.swift
@@ -8,12 +8,31 @@
 
 import UIKit
 
+///< 底部选择view的代理
 protocol YF_StockChartSegmentViewDelegate: NSObjectProtocol {
     func clickSegmentButton(index: Int?, chartSegmentView: YF_StockChartSegmentView?)
 }
 
 class YF_StockChartSegmentView: UIView {
+    
+    weak var delegate: YF_StockChartSegmentViewDelegate?
+    
     var items: [Any]?
     
     var selectedIndex: Int?
+    
+    convenience init(items: [Any]) {
+        self.init(frame: .zero)
+        self.items = items
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        backgroundColor = ASSISTANT_BACKGROUND_COLOR
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
