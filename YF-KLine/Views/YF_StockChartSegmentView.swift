db9b0b9462dc05981958d4a3695da31399af1538
tsaievan <tsaievan@icloud.com>
2018-02-28 13:37:37 +0800

    加了一些注释

--- YF-KLine/Views/YF_StockChartSegmentView.swift
+++ YF-KLine/Views/YF_StockChartSegmentView.swift
@@ -8,24 +8,28 @@
 
 import UIKit
 
-///< 底部选择view的代理
+///< 底部选择view的代理协议
 protocol YF_StockChartSegmentViewDelegate: NSObjectProtocol {
     func clickSegmentButton(index: Int?, chartSegmentView: YF_StockChartSegmentView?)
 }
 
 class YF_StockChartSegmentView: UIView {
-    
+    ///< 代理属性
     weak var delegate: YF_StockChartSegmentViewDelegate?
     
+    ///< segmentView的模型数组
     var items: [Any]?
     
+    ///< 选择的index
     var selectedIndex: Int?
     
+    ///< 便利构造函数
     convenience init(items: [Any]) {
         self.init(frame: .zero)
         self.items = items
     }
-
+    
+    ///< 重写指定构造函数
     override init(frame: CGRect) {
         super.init(frame: frame)
         clipsToBounds = true
